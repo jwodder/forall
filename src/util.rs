ae5441d37d5d1eef87d603332d15d5b9ecc59177
John T. Wodder II <git@varonathe.org>
2024-12-11 22:13:40 -0500

    Use anstyle for colors and anstream for uncoloring when piped

--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,13 @@
+use anstyle::{AnsiColor, Style};
+
 pub(crate) fn printlnbold(s: &str) {
-    println!("\x1B[1m{s}\x1B[m");
+    printlnstyled(s, Style::new().bold());
 }
 
 pub(crate) fn printlnerror(s: &str) {
-    println!("\x1B[1;31m{s}\x1B[m");
+    printlnstyled(s, Style::new().fg_color(Some(AnsiColor::Red.into())).bold());
+}
+
+fn printlnstyled(s: &str, style: Style) {
+    anstream::println!("{style}{s}{style:#}");
 }
